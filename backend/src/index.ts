ebc94cdbf2d98ac6330786cd61fc613862db7603
miguel-nascimento <miguelgomes13@live.com>
2021-06-02 19:04:36 -0300

    add [backend] static file serving

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,18 +3,22 @@ import express from 'express'
 import { BookResolver } from './resolvers/book'
 import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
+import * as path from 'path'
 import { createConnection } from 'typeorm'
 
 const PORT = 42069
 const main = async () => {
     const db = await createConnection()
     const app = express()
+
     const apollo = new ApolloServer({
         schema: await buildSchema({
             resolvers: [BookResolver],
             validate: false,
         }),
     })
+
+    app.use('/static', express.static(path.resolve(__dirname, '..', 'uploads')))
     apollo.applyMiddleware({ app })
     app.listen(PORT, () => {
         console.log(`Express file serving running in http://localhost:${PORT}`)
