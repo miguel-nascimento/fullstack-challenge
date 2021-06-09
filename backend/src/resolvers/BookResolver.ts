5a6834260afae3e50cc1fe16cadc6b43a062296c
miguel-nascimento <miguelgomes13@live.com>
2021-06-09 20:37:44 -0300

    change [backend] image upload obj

--- backend/src/resolvers/BookResolver.ts
+++ backend/src/resolvers/BookResolver.ts
@@ -38,7 +38,7 @@ class BookResolver {
   ): Promise<boolean | undefined> {
     console.log(input.image)
     if (input.image) {
-      const hashedName = `${uuidv4()}-${input.image.file.filename}`
+      const hashedName = `${uuidv4()}-${input.image.file.name}`
       return new Promise((resolve, reject) =>
         input.image.file
           .createReadStream()
